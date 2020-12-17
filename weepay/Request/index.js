8901f401e1e2e0630767e78950e3340d708f99fd
farukozdemirz <frk.ozdmrr@gmail.com>
2020-12-17 16:28:55 +0300

    bug fix

--- weepay/Request/index.js
+++ weepay/Request/index.js
@@ -1,6 +1,3 @@
-const Address = require("./Address");
-const Customer = require("./Customer");
-const Products = require("./Products");
 const Auth = require("./Auth");
 const FormInitializeRequest = require("./FormInitializeRequest");
 const GetPaymentRequest = require("./GetPaymentRequest");
@@ -8,9 +5,6 @@ const CreatePaymentRequest = require("./CreatePaymentRequest");
 const CreatePaymentRequestThreeD = require("./CreatePaymentRequestThreeD");
 
 module.exports = {
-    Address,
-    Customer,
-    Products,
     Auth,
     FormInitializeRequest,
     GetPaymentRequest,
